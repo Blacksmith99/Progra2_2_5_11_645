f2ae8008de9a6c91ac8f197e51128ddaed058845
unknown <Josue@.(none)>
2011-10-07 08:57:05 +0200

    Este es mi primer colaboracion con el proyecto

---
+++ src/progra2_2_5_11_645/Claseprueba.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package progra2_2_5_11_645;
+
+/**
+ *
+ * @author Josue
+ */
+public class Claseprueba {
+    
+    public static void main(String arg[]){
+        
+        System.out.print("Hola Mundo");
+        
+    }
+    
+}
